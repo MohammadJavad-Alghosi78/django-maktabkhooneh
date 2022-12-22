56cfb4a123ae92bf13fb27bd5f4dd54d4fd3bd57
Milad <javad.alghosi7904@gmail.com>
2022-12-22 15:35:01 +0330

    second task from db season

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', blog_view, name="index"),
-    path('single/<int:pid>', blog_single, name="single"),
+    path('<int:pid>', blog_single, name="single"),
     # path('<str:name>/<str:family_name>/<int:age>', test, name="test")
-    path('post-<int:pid>', test, name="test")
+    # path('post-<int:pid>', test, name="test")
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,15 +6,29 @@ from django.db.models import F
 
 # Create your views here.
 def blog_view(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now())
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=1)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
 def blog_single(request, pid):
-    post = get_object_or_404(Post, pk=pid)
-    post.counted_view += 1
-    post.save()
-    context = {'post': post }
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=1)
+    current_post = get_object_or_404(Post, pk=pid, status=1)
+    current_post.counted_view += 1
+    current_post.save()
+    
+    prev_post = {}
+    next_post = {}
+    for post_index in range(len(posts)):
+        if(current_post.id == posts[post_index].id):
+            if(post_index == 0):
+                next_post = posts[post_index + 1]
+            elif(post_index == len(posts) - 1):
+                prev_post = posts[post_index - 1]
+            else:
+                next_post = posts[post_index + 1]
+                prev_post = posts[post_index - 1]
+    
+    context = {'post': current_post, 'prev_post': prev_post, 'next_post': next_post }
     return render(request, 'blog/blog-single.html', context)
 
 def test(request, pid):
