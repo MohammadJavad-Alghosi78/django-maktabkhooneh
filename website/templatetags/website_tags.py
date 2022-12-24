19ff265ac9b25b1261899ba048feb6f2870f57a6
Milad <javad.alghosi7904@gmail.com>
2022-12-24 15:08:37 +0330

    start form

--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -6,7 +6,6 @@ register = template.Library()
 @register.inclusion_tag('website/website-latest-posts.html')
 def landinglatestposts():
     posts = Post.objects.filter(status=1)[:6]
-    print(len(posts))
     return {'posts': posts}
 
 @register.filter
--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from website.forms import NameForm, ContactForm, NewsletterForm
+from django.contrib import messages
+
 
 # Create your views here.
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -10,7 +13,34 @@ def about_view(request):
     return render(request, 'website/about.html')
 
 def contact_view(request):
-    return render(request, 'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Your ticket submitted successfully')
+        else:
+            messages.add_message(request, messages.ERROR, 'Your ticket did not submitted')
+    
+    form = ContactForm()
+    return render(request, 'website/contact.html', {'form': form})
+
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    return HttpResponseRedirect('/')
 
 def test_view(request):
-    return render(request, 'website/test.html', {'name': "Milad", 'lastname': 'Alghosi'})
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('done')
+        else:
+            return HttpResponse('not valid')
+    
+    form = ContactForm()
+    return render(request, 'website/test.html', {'form': form})
+
